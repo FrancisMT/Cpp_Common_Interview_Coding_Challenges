e2592531860b8826c2c7f11ea297b96181e736ad
Francisco de Babo Martins <x2francisco@gmail.com>
2020-05-06 19:39:01 +0100

    Update FirstNonRepeatingCharacter.cpp

--- 30_FirstNonRepeatingCharacter.cpp
+++ 30_FirstNonRepeatingCharacter.cpp
@@ -22,9 +22,10 @@ int findIndex(const std::string& inString) {
 
     for(int i = 0; i < inString.size(); ++i) {
 
-        auto findResult = std::find_if(seenChars.begin(), seenChars.end(), [character = inString[i]](const std::pair<char,int>& currentPair ){
-            return currentPair.first == character;
-        });
+        auto findResult = std::find_if(seenChars.begin(), seenChars.end(), 
+            [character = inString[i]](const std::pair<char,int>& currentPair ){
+                return currentPair.first == character;
+            });
 
         if(findResult == seenChars.end()){
             seenChars.insert({inString[i], i});
@@ -34,9 +35,10 @@ int findIndex(const std::string& inString) {
         }
     }
 
-    auto result = std::min_element(seenChars.begin(), seenChars.end(), [](std::pair<char, int> a, std::pair<char, int> b){
-        return a.second < b.second;
-    });
+    auto result = std::min_element(seenChars.begin(), seenChars.end(),
+        [](std::pair<char, int> a, std::pair<char, int> b){
+            return a.second < b.second;
+        });
 
     return result == seenChars.end() ? -1 : result->second;
 }
--- README.md
+++ README.md
@@ -84,3 +84,10 @@ Find the squareroot of a given number rounded down to the nearest integer, witho
 
 ### 28. Longest Compound Word
 Given a sorted list of words, find the longest compound word in the list that is constructed by concatenating the words in the list. For example, if the input list is: [‘cat’, ‘cats’, ‘catsdogcats’, ‘catxdogcatsrat’, ‘dog’, ‘dogcatsdog’, ‘hippopotamuses’, ‘rat’, ‘ratcat’, ‘ratcatdog’, ‘ratcatdogcat’]. Then the longest compound word is ‘ratcatdogcat’ with 12 letters. Note that the longest individual words are ‘catxdogcatsrat’ and ‘hippopotamuses’ with 14 letters, but they’re not fully constructed by other words. Former one has an extra ‘x’ letter, and latter is an individual word by itself not a compound word.
+
+### 29. Find Unique Non Duplicate Number
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+### 30. First Non Repeating Character
+Given a string, find the first non-repeating character in it and return it's index.
+If it doesn't exist, return -1.
